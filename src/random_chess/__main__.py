ab3ccde4022648b5ac8321b6066002e5e02ce277
Jeremy Brandow <68441282+Jampamane@users.noreply.github.com>
2025-09-18 15:23:09 +0000

    fix: changed from forever chess to 1 game

--- src/random_chess/__main__.py
+++ src/random_chess/__main__.py
@@ -9,12 +9,8 @@ def random_chess() -> None:
     and then plays the chess game.
     """
     game = Game(headless=True)
-    while True:
-        try:
-            game.play_game(game_type="1 min")
-        except KeyboardInterrupt:
-            print("Stopping chess bot!")
-            break
+
+    game.play_game(game_type="1 min")
 
 
 if __name__ == "__main__":
